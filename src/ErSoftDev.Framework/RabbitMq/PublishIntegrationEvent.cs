f4c6cb5c919443846e6a9912be2b4140fa36c0ee
ehsan rezaee <ehsan.rezaee021@gmail.com>
2024-07-27 16:40:31 +0330

    feat : Add custom IntegrationEventBus to publish integration event by cap

---
+++ src/ErSoftDev.Framework/RabbitMq/PublishIntegrationEvent.cs
@@ -0,0 +1,54 @@
+﻿using DotNetCore.CAP;
+
+namespace ErSoftDev.Framework.RabbitMq
+{
+    public class PublishIntegrationEvent<T> : ICapPublisher
+    {
+        public Task PublishAsync<T>(string name, T? contentObj, string? callbackName = null,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PublishAsync<T>(string name, T? contentObj, IDictionary<string, string?> headers,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Publish<T>(string name, T? contentObj, string? callbackName = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Publish<T>(string name, T? contentObj, IDictionary<string, string?> headers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PublishDelayAsync<T>(TimeSpan delayTime, string name, T? contentObj, IDictionary<string, string?> headers,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PublishDelayAsync<T>(TimeSpan delayTime, string name, T? contentObj, string? callbackName = null,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PublishDelay<T>(TimeSpan delayTime, string name, T? contentObj, IDictionary<string, string?> headers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PublishDelay<T>(TimeSpan delayTime, string name, T? contentObj, string? callbackName = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IServiceProvider ServiceProvider { get; }
+        public AsyncLocal<ICapTransaction> Transaction { get; }
+    }
+}
